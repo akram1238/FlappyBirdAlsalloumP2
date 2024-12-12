f9b08444f5d27058f0c480ec18c4d59a2cfb341a
akram1238 <754043@student.dusd.net>
2024-12-11 18:28:40 -0800

    I created 2 scripts
    one for coloums and one for the repeating background

--- P2AAlsalloum/Assets/Scripts/GameController.cs
+++ P2AAlsalloum/Assets/Scripts/GameController.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public static GameController instance;
     public GameObject gameOverText;
+    public Text scoretext;
     public bool gameOver = false;
     public float scrollSpeed = -1.5f;
+
+    private int score = 0;
 
     // Start is called before the first frame update
     void Awake()
@@ -30,6 +34,15 @@ public class GameController : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+    }
+    public void BirdScored()
+    {
+       if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoretext.text = "score" + score.ToString();
     }
     
     public void BirdDied()
